8ce0b4f987350b83a002a33a3c0670f777d02eaa
cashleysaintilus@gmail.com <cashleysaintlus@gmail.com>
2018-01-06 20:29:19 -0500

    I started my code over but im on track now

--- script.js
+++ script.js
@@ -1,73 +1,122 @@
-$(function() {
-    // This is  a Constructor Functions. It allow me to create more object
-    var Question = function(fieldtext, choices, answer) {
-        this.fieldtext = fieldtext;
-        this.choices = choices;
-        this.answer = answer;
-    }
-    //make sure the choice of user is equal  ot the answer
-    Question.prototype.correctAnswer = function(choice) {
-        return choice === this.answer;
-    }
-    var the_questions = [
-        new Question('Who is the best all-around player in the NBA ?', ['Kobe Bryant', 'Michael Jordan', 'Steve Kerr', "Lebron James, Obvioustly"], 'Lebron James, Obvioustly'),
-        new Question('There was a plane crash every single person died. Who survived?', ['the plane', 'No One, Genius', 'Married Couple,duh', 'The pilot '], 'Married Couple,duh'),
-        new Question('Which language is used to style on them?', ['Java', 'Javascript', 'Css', 'That Money, duh'], 'Css'),
-        new Question('Winter Is coming?', ['Games of Thrones', 'John Wick', 'Hercules', 'None of them'], 'Games of Thrones'),
-        new Question('The more you take, the more you leave behind. What am I?', ['Life', 'Shadow', 'Babies', "footstep, Obvioustly"], 'footstep'),
-    ];
 
-    function Quiz(questionAsk) {
-        this.score = 0;
-        this.questionAsk = questionAsk;
-        this.questionIndex = 0;
-    }
-    // we get the index of the questions
-    Quiz.prototype.getIndex = function() {
-        return this.questionAsk[this.questionIndex];
-    }
-    // check if the quiz has ended
-    Quiz.prototype.check_if_quiz_ended = function() {
-        return this.questionAsk.length === this.questionIndex;
+var questionIndex = 0;
+var score = 0;
+// This is  a Constructor Functions. It allow me to create more object
+var Question = function(quizQuestion, choices, answer) {
+    this.quizQuestion = quizQuestion;
+    this.choices = choices;
+    this.answer = answer;
+}
+var the_questions = [
+    new Question('Who is the best all-around player in the NBA ?', ['Kobe Bryant', 'Michael Jordan', 'Steve Kerr', "Lebron James, Obvioustly"], 'Lebron James, Obvioustly'),
+    new Question('There was a plane crash every single person died. Who survived?', ['the plane', 'No One, Genius', 'Married Couple,duh', 'The pilot '], 'Married Couple,duh'),
+    new Question('Which language is used to style on them?', ['Java', 'Javascript', 'Css', 'That Money, duh'], 'Css'),
+    new Question('Winter Is coming?', ['Games of Thrones', 'John Wick', 'Hercules', 'None of them'], 'Games of Thrones'),
+    new Question('The more you take, the more you leave behind. What am I?', ['Life', 'Shadow', 'Babies', 'footstep, Obvioustly'], 'footstep, Obvioustly')
 
-    }
-    // if the user pick the right answer increase the score by 1 and 
-    // Also, move on to the next  question whether the user got it right or not
-    Quiz.prototype.next_question = function(element) {
-        var index = this.getIndex();
-        var the_answer = correctAnswer(element);
-        if (index.the_answer) {
-            this.score++;
-        }
-        this.questionIndex++;
-    }
-    //create a new quiz object everytime there is one until its finish
-    var test = new Quiz(the_questions);
+];
 
-    function getQuestion() {
-        if (test.check_if_quiz_ended()) {
-            // showScore();
+var scores = [the_questions.length];
+
+$(document).ready(function() {
+    showQuestion();
+    showAnswers();
+    // $('.next').on('click',next());
+    $('.answer').on('click', function() {
+        if ($(this).text() == the_questions[questionIndex].answer) {
+            scores[questionIndex] = true;
         } else {
-            // show question and answers
-            var mainQuestion = $('#questions');
-            var putAnswer = $('.answer');
-            var q = test.getIndex();
-            for (var i = 0; i < the_questions.length; i++) {
-                mainQuestion.text(q.fieldtext);
-            }
-            for (var i = 0; i < putAnswer.length; i++) {
-                $(putAnswer[i]).text(q.choices[i]);
-            }
-        }
-        // $(putAnswer[i]).on('click',function(heoo){
-        //     alert('hello')
-        // })
-    }
-    // function showAnswer(){
-    //     var putAnswer = $('.answer');
-    //     putAnswer.text(test.getIndex().choices)
-    // }
-    getQuestion();
+            scores[questionIndex] = false;
+        } 
+ 
 
-
+    });
+    $('.submit').on('click',function(){
+        computeScore();
+    })
 });
+
+function showQuestion() {
+    $('#question').text(the_questions[questionIndex].quizQuestion);
+    // alert(questionIndex);
+}
+
+function showAnswers() {
+    $('#choice1').text(the_questions[questionIndex].choices[0]);
+    $('#choice2').text(the_questions[questionIndex].choices[1]);
+    $('#choice3').text(the_questions[questionIndex].choices[2]);
+    $('#choice4').text(the_questions[questionIndex].choices[3]);
+
+}
+
+function next() {
+    if (this.questionIndex < the_questions.length - 1) {
+        this.questionIndex++;
+        showQuestion();
+        showAnswers();
+        $('.previous').prop('disabled', false);
+
+    } else {
+        $('.next').prop('disabled', true);
+    }
+
+
+}
+
+
+function previous() {
+    if (this.questionIndex > 0) {
+        this.questionIndex--;
+        showQuestion();
+        showAnswers();
+        $('.next').prop('disabled', false);
+
+    } else {
+        $('.previous').prop('disabled', true);
+
+    }
+}
+
+
+function computeScore() {
+ var score = 0;
+for(var i = 0; i < scores.length; i++){
+    if (scores[i] ==true) {
+        score++;
+    } 
+}
+$('.result').text(score);
+
+}
+
+
+
+
+
+
+
+// function changeQuestion(id){
+//     if (id == "previous") {
+//        if (this.questionIndex > 0) {
+//         this.questionIndex--;
+//         showQuestion();
+//         showAnswers();
+//         $('.next').prop('disabled', false);
+
+//     } else {
+//         $('.previous').prop('disabled', true); 
+//     }
+// } else {
+//      if (this.questionIndex < the_questions.length - 1) {
+//         this.questionIndex++;
+//         showQuestion();
+//         showAnswers();
+//         $('.previous').prop('disabled', false);
+
+//     } else {
+//         $('.next').prop('disabled', true);
+//     }
+
+// }
+// }
+
